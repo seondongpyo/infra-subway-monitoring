4f16dbcf3f55c3b7c89f29ea22b97e13745779cc
seondongpyo <seondongpyo@gmail.com>
2021-06-15 17:18:11 +0900

    refactor: AOP 수정

--- src/main/java/nextstep/subway/aop/LogAop.java
+++ src/main/java/nextstep/subway/aop/LogAop.java
@@ -17,20 +17,19 @@ public class LogAop {
 
     private final Logger log = LoggerFactory.getLogger(LogAop.class);
 
-//    @Around("execution(* nextstep.subway.line.application.LineService.*(..))")
-//    @Around("execution(* nextstep.subway.*.*(..))")
-//    public void line(ProceedingJoinPoint joinPoint) throws Throwable {
-//        StopWatch stopWatch = new StopWatch();
-//        stopWatch.start();
-//
-//        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-//        String methodName = methodSignature.getMethod().getName();
-//        String arguments = Arrays.toString(joinPoint.getArgs());
-//        log.info("{} | {}", methodName, arguments);
-//        joinPoint.proceed();
-//
-//        stopWatch.stop();
-//        log.info("total taken time : {}", stopWatch.getTotalTimeSeconds());
-//    }
+   @Around("execution(* nextstep.subway.line.application.LineService.*(..))")
+   public void line(ProceedingJoinPoint joinPoint) throws Throwable {
+       StopWatch stopWatch = new StopWatch();
+       stopWatch.start();
+
+       MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+       String methodName = methodSignature.getMethod().getName();
+       String arguments = Arrays.toString(joinPoint.getArgs());
+       log.info("{} | {}", methodName, arguments);
+       joinPoint.proceed();
+
+       stopWatch.stop();
+       log.info("total taken time : {}", stopWatch.getTotalTimeSeconds());
+   }
 
 }
