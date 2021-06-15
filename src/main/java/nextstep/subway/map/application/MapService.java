10a7915af8c0bce85653804207db2a4ed9869fe2
seondongpyo <seondongpyo@gmail.com>
2021-06-15 17:53:10 +0900

    feat: 로깅 추가

--- src/main/java/nextstep/subway/map/application/MapService.java
+++ src/main/java/nextstep/subway/map/application/MapService.java
@@ -1,5 +1,15 @@
 package nextstep.subway.map.application;
 
+import static net.logstash.logback.argument.StructuredArguments.*;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import io.jsonwebtoken.lang.Collections;
 import nextstep.subway.line.application.LineService;
 import nextstep.subway.line.domain.Line;
 import nextstep.subway.map.domain.SubwayPath;
@@ -7,14 +17,12 @@ import nextstep.subway.map.dto.PathResponse;
 import nextstep.subway.map.dto.PathResponseAssembler;
 import nextstep.subway.station.application.StationService;
 import nextstep.subway.station.domain.Station;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @Transactional
 public class MapService {
+
+    private final Logger log = LoggerFactory.getLogger("json");
     private LineService lineService;
     private StationService stationService;
     private PathService pathService;
@@ -30,6 +38,12 @@ public class MapService {
         Station sourceStation = stationService.findById(source);
         Station targetStation = stationService.findById(target);
         SubwayPath subwayPath = pathService.findPath(lines, sourceStation, targetStation);
+
+        log.info("Find a path from {} to {} = {} ({})",
+            kv("source", sourceStation),
+            kv("target", targetStation),
+            kv("distance", subwayPath.calculateDistance()),
+            kv("stations", subwayPath.getStations()));
 
         return PathResponseAssembler.assemble(subwayPath);
     }
